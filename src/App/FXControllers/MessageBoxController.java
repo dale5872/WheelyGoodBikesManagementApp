d76856823b01beca3c62b7437bad0bb6adc35d67
CPairman <calum.pairman@gmail.com>
2019-03-17 20:05:12 +0000

    Message boxes will now resize to fit the entire label text

--- src/App/FXControllers/MessageBoxController.java
+++ src/App/FXControllers/MessageBoxController.java
@@ -6,11 +6,10 @@ import javafx.scene.control.Label;
 public class MessageBoxController extends Controller{
     @FXML private Label messageLabel;
 
-    private String message;
-
     public void setMessage(String message) {
-        this.message = message;
         messageLabel.setText(message);
+        stage.sizeToScene();
+        stage.centerOnScreen();
     }
 
     public void setAlwaysOnTop(boolean b){
