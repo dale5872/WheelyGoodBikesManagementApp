8876e7c7fad6727cd67ae311d233dd01a43f55a2
CPairman <calum.pairman@gmail.com>
2019-03-17 01:56:26 +0000

    Implemented file browser for selecting bike/equipment type images

--- src/App/FXControllers/AddEditTypeController.java
+++ src/App/FXControllers/AddEditTypeController.java
@@ -1,13 +1,15 @@
 package App.FXControllers;
 
 import App.Classes.Type;
-import javafx.event.Event;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.BlurType;
 import javafx.scene.effect.InnerShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
@@ -24,29 +26,83 @@ public class AddEditTypeController extends PopupController{
     @FXML private Label allFieldsWarning;
 
     @FXML private TextField nameField;
+
+    @FXML private StackPane dragBox;
+    @FXML private ImageView imageDisplay;
     @FXML private TextField imageUrlField;
+
     @FXML private TextField priceField;
 
     @FXML private Label priceWarning;
 
     @FXML private Button confirmBtn;
 
-    @FXML private StackPane dragBox;
-
+    /**
+     * Set whether we're adding/updating a bike type or equipment type
+     * @param isBikeType
+     */
     public void setIsBike(Boolean isBikeType){
         this.isBikeType = isBikeType;
     }
 
+    /**
+     * Set the existing bike/equipment type
+     * @param type
+     */
     public void setExistingType(Type type){
         this.existingType = type;
 
         nameField.setText(this.existingType.getName());
-        imageUrlField.setText(this.existingType.getImage());
+        displayImage(this.existingType.getImage());
         priceField.setText(Double.toString(this.existingType.getPrice()));
 
         confirmBtn.setText("Update");
     }
 
+    /**
+     * Displays an image from a given path or URL
+     * @param imagePath The path/URL to the image
+     */
+    private void displayImage(String imagePath){
+        Image image;
+
+        boolean isUrl = (imagePath.startsWith("http:")) || (imagePath.startsWith("https:"));
+
+        if(isUrl){
+            image = new Image(imagePath);
+        }else{
+            File file = new File(imagePath);
+            image = new Image(file.toURI().toString());
+        }
+
+        imageDisplay.setImage(image);
+        imageUrlField.setText(imagePath);
+    }
+
+    /**
+     * Opens an image browser and displays the chosen image on the form
+     */
+    @FXML
+    protected void chooseImage(){
+        String title;
+        if(existingType == null){
+            title = "New Type Image";
+        }else{
+            title = "Edit Type Image";
+        }
+
+        ImageBrowser imageBrowser = new ImageBrowser(title);
+        imageBrowser.showBrowser(stage);
+        String imagePath = imageBrowser.getImagePath();
+
+        if(imagePath != null) {
+            displayImage(imagePath);
+        }
+    }
+
+    /**
+     * Creates/updates the type and passes it back to the parent controller
+     */
     @FXML
     protected void confirm(){
         if(validateInput() == true){
@@ -85,55 +141,9 @@ public class AddEditTypeController extends PopupController{
     }
 
     /**
-     * Gets the filename from the dragged file and updates the filename box
-     * @param e DragEvent
+     * Checks that all fields have a value and that the price is of a valid format
+     * @return
      */
-    @FXML
-    protected void getFilename(DragEvent e) {
-
-        Boolean success = false;
-
-        Dragboard board = e.getDragboard();
-        if(board.hasFiles()) {
-            File file = board.getFiles().get(0);
-            String filename = file.getAbsolutePath();
-
-            imageUrlField.setText(filename);
-
-            success = true;
-
-        } else {
-            System.err.println("No files dropped!");
-        }
-
-        e.setDropCompleted(success);
-        e.consume();
-    }
-
-    @FXML
-    protected void dragOver(DragEvent e) {
-        dragBox.setEffect(new InnerShadow(BlurType.THREE_PASS_BOX, Color.WHITE,2,0,0,0));
-
-        Dragboard board = e.getDragboard();
-
-        boolean isAccepted = board.getFiles().get(0).getName().toLowerCase().endsWith(".png")
-                || board.getFiles().get(0).getName().toLowerCase().endsWith(".jpeg")
-                || board.getFiles().get(0).getName().toLowerCase().endsWith(".jpg");
-
-        if(board.hasFiles()) {
-            if(isAccepted) {
-                e.acceptTransferModes(TransferMode.LINK);
-            }
-        } else {
-            e.consume();
-        }
-    }
-
-    @FXML
-    protected void dragExited(DragEvent e) {
-        dragBox.setEffect(new InnerShadow(BlurType.THREE_PASS_BOX, Color.WHITE,0,0,0,0));
-    }
-
     private boolean validateInput(){
         boolean hasBlankFields = checkForBlankFields();
         boolean priceIsDouble = checkPriceIsDouble();
@@ -187,5 +197,55 @@ public class AddEditTypeController extends PopupController{
         }catch(NumberFormatException e){
             return false;
         }
+    }
+
+    /**
+     * Gets the filename from the dragged file and updates the filename box
+     * @param e DragEvent
+     */
+    @FXML
+    protected void getFilename(DragEvent e) {
+        Boolean success = false;
+
+        Dragboard board = e.getDragboard();
+        if(board.hasFiles()) {
+            File file = board.getFiles().get(0);
+            String filename = file.getAbsolutePath();
+
+            displayImage(filename);
+            imageUrlField.setText(filename);
+
+            success = true;
+
+        } else {
+            System.err.println("No files dropped!");
+        }
+
+        e.setDropCompleted(success);
+        e.consume();
+    }
+
+    @FXML
+    protected void dragOver(DragEvent e) {
+        dragBox.setEffect(new InnerShadow(BlurType.THREE_PASS_BOX, Color.WHITE,2,0,0,0));
+
+        Dragboard board = e.getDragboard();
+
+        boolean isAccepted = board.getFiles().get(0).getName().toLowerCase().endsWith(".png")
+                || board.getFiles().get(0).getName().toLowerCase().endsWith(".jpeg")
+                || board.getFiles().get(0).getName().toLowerCase().endsWith(".jpg");
+
+        if(board.hasFiles()) {
+            if(isAccepted) {
+                e.acceptTransferModes(TransferMode.LINK);
+            }
+        } else {
+            e.consume();
+        }
+    }
+
+    @FXML
+    protected void dragExited(DragEvent e) {
+        dragBox.setEffect(new InnerShadow(BlurType.THREE_PASS_BOX, Color.WHITE,0,0,0,0));
     }
 }
---
+++ src/App/FXControllers/ImageBrowser.java
@@ -0,0 +1,42 @@
+package App.FXControllers;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class ImageBrowser {
+    private final FileChooser fileChooser;
+    private File image;
+
+    /**
+     * Creates a file chooser that can select .jpeg, .jpg, or .png files
+     * @param title The title of the browser
+     */
+    public ImageBrowser(String title){
+        fileChooser = new FileChooser();
+        fileChooser.setTitle(title);
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Images", "*.jpeg", "*.jpg", "*.png"),
+                new FileChooser.ExtensionFilter("JPEG", "*.jpeg"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+    }
+
+    /**
+     * Shows the file chooser
+     * @param stage The stage of the browser
+     */
+    public void showBrowser(Stage stage){
+        image = fileChooser.showOpenDialog(stage);
+    }
+
+    /**
+     * Gets the path of the image chosen in the browser
+     * @return A String of the absolute path to the image, NULL if no image has been chosen
+     */
+    public String getImagePath(){
+        return this.image.getAbsolutePath();
+    }
+}
